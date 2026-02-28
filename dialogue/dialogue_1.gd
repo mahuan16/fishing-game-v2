6288dcd26fcd38944345468b1e8265fe6ac348c6
sm02-08 <shelleyma02@gmail.com>
2026-02-28 17:44:29 -0500

    dialogue1 finished

---
+++ dialogue/dialogue_1.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+#if InputEventMouse():
+#
+var resource = preload("res://dialogue/balloon.tscn")
+#
+#DialogueManager.show_dialogue_balloon(resource, "start")
+## var dialogue1 = await DialogueManager.get_next_dialogue_line(resource, "start")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"): 
+		var balloon: BaseGameDialogueBalloon = resource.instantiate()
+		get_tree().root.add_child(balloon)
+		balloon.start(load("res://dialogue/dialogue1.dialogue"), "start")
+		#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/fisher_text.dialogue"), "start")
+		#return 
+
+#
+#func _unhandled_input(event: InputEvent) -> void: 
+	#if in_range: 
+		#if event.is_action_pressed("show_dialogue"): 
+			#var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate() 
+			#get_tree().root.add_child(balloon) 
+			#balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
