ff15e9f389e76b691f2b6a28684b32543e07bf7a
sm02-08 <shelleyma02@gmail.com>
2026-02-28 19:35:10 -0500

    ocean bg + bubbles + some edits to buttons

---
+++ globals/dialogue_trueornot.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name Globals
+
+static var dialogue_enabled : bool = true  
--- scripts/button.gd
+++ scripts/button.gd
@@ -2,6 +2,8 @@ extends Button
 
 @onready var game_manager: Node = %gameManager
 
+ 
 
 func _on_pressed() -> void:
 	game_manager.start_fishing()
+	Globals.dialogue_enabled = false 
