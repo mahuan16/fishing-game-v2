61371a427b713a9843a01df3a960da334d0357de
AnabelleA <anabelleahteck@gmail.com>
2026-02-28 19:30:36 -0500

    updated fish entry

--- scripts/fish_entry.gd
+++ scripts/fish_entry.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+const FISH_SCENE = preload("uid://ptd54wdcx6by")
+
+var fish : Node
+@export var data : FishData
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	fish = FISH_SCENE.instantiate()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+func set_data():
+	fish.data = data
+	fish.set_data()
+
+	
+	
+
+	
+	
